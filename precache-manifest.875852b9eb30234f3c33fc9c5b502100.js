5b75da2bc17b91b135cdb419d3bb130baedac621
bepul0 <bepul.cse.ru@gmail.com>
2020-10-28 09:54:45 +0600

    Updates

---
+++ precache-manifest.875852b9eb30234f3c33fc9c5b502100.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8623444718394c148e8bd2e3ba9b57f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fab3da8e7da055b78912",
+    "url": "/static/css/main.f98749f7.chunk.css"
+  },
+  {
+    "revision": "9dd32b90052f2465e6f7",
+    "url": "/static/js/2.20b43afa.chunk.js"
+  },
+  {
+    "revision": "3a5da4106a08e596432e5e2628e1ef8d",
+    "url": "/static/js/2.20b43afa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fab3da8e7da055b78912",
+    "url": "/static/js/main.52533bff.chunk.js"
+  },
+  {
+    "revision": "c9913191e565de632244",
+    "url": "/static/js/runtime-main.170f8412.js"
+  },
+  {
+    "revision": "c93975e3e5fc5c6bd4d2e789ff31ae4a",
+    "url": "/static/media/cork-wallet.c93975e3.png"
+  },
+  {
+    "revision": "b542820569fafa072d5e924565609404",
+    "url": "/static/media/desktop13.b5428205.jpg"
+  }
+]);
